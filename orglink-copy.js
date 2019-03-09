60373fd847edc7bea19c0d65b4be989dc42f20b4
Koji Matsuda <kjmatsuda1985@gmail.com>
2019-03-09 10:43:01 +0900

    Add comments. Firefox doesn't permit copy to clipbord, so display the text on prompt.

--- orglink-copy.js
+++ orglink-copy.js
@@ -1,3 +1,7 @@
+// Author:  Koji Matsuda
+// Created: 2019/3/9
+// Comment: Copy url and title of page to clipbord as org link format.
+//          To Install on firefox, you must remove new line. (M-% C-q C-j enter enter)
 javascript:(function()
             {
                 function copyTextToClipboard(textVal){
@@ -10,11 +14,13 @@ javascript:(function()
                     bodyElm.removeChild(copyFrom);
                     return retVal;
                 }
-                var prefix="";
-                var suffix="";
+                function displayOnPrompt(textVal) {
+                    prompt("", textVal);
+                }
                 var contents = document.title;
-                var target="_blank";
-                var succeed =  copyTextToClipboard(prefix+"[[" + location.href + "][" + contents + "]]" + suffix);
+                var orgLink = "[[" + location.href + "][" + contents + "]]";
+                var succeed =  copyTextToClipboard(orgLink);
+                if (!succeed) {
+                    displayOnPrompt(orgLink);
+                }
             })();
-
-
